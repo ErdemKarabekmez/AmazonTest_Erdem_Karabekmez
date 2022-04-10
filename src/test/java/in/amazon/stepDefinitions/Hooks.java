8c4968ce1651dfc3175b708ab5d6f21c62cecb7f
Erdem KARABEKMEZ <erdemkarabekmez@gmail.com>
2022-04-10 17:59:34 +0300

    Hooks added

---
+++ src/test/java/in/amazon/stepDefinitions/Hooks.java
@@ -0,0 +1,46 @@
+package in.amazon.stepDefinitions;
+
+import in.amazon.utilities.ConfigurationReader;
+import in.amazon.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+    /**
+     * These lines will be executed before every test
+     */
+    @Before
+    public void setUp() {
+
+
+        Driver.get().get(ConfigurationReader.get("url"));
+        Driver.get().manage().window().maximize();
+        Driver.get().manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    /**
+     * These lines will be executed after every test
+     * Screenshot will be captured if the scenario fails
+     *
+     * @param scenario
+     */
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()){
+            final byte [] screenshot = ((TakesScreenshot)Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.close();
+    }
+}
+
+
+
+
